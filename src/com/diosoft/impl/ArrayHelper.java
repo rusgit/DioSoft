7ca2c74eb07d4a79d7019d5b9fbc40cbc0b51580
Ruslan <borisov.rusya@gmail.com>
2014-09-02 10:40:28 +0300

    Update ArrayHelper.java

--- src/com/diosoft/impl/ArrayHelper.java
+++ src/com/diosoft/impl/ArrayHelper.java
@@ -17,17 +17,6 @@ public class ArrayHelper implements JoinOperationByArray {
         int firstArrayLength = getLength(firstArray);
         int secondArrayLenght = getLength(secondArray);
 
-        if (firstArrayLength == 0)
-            return deleteDuplicate(secondArray);
-
-        if (secondArrayLenght == 0)
-            return deleteDuplicate(firstArray);
-            
-        if (secondArrayLenght == 0)
-            return deleteDuplicate(firstArray);
-
-        if (firstArrayLength == 0)
-           return deleteDuplicate(secondArray);
 
         Person[] mergeArray = new Person[firstArray.length + secondArray.length];
 // Add all elements from First Array and Second Array
